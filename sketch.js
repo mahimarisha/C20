916d0d9f3e326543e5a0205e5101eb4030573e82
mahimarisha <77223385+mahimarisha@users.noreply.github.com>
2021-01-27 16:32:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var a,b;
+
+function setup() {
+  createCanvas(800,400);
+  a=createSprite(400, 200, 50, 50);
+  b=createSprite(200,200,40,40);
+  a.shapeColor="green";
+  b.shapeColor="green";
+}
+
+function draw() {
+  background("pink"); 
+  a.x=World.mouseX;
+  a.y=World.mouseY; 
+  if (a.x-b.x<a.width/2+b.width/2&&b.x-a.x<a.width/2+b.width/2&&a.y-b.y <a.height/2+b.height/2&&b.y-a.y<b.height/2+a.height/2) {
+    a.shapeColor="blue";
+  b.shapeColor="blue";
+  }
+  else {
+    a.shapeColor="green";
+    b.shapeColor="green";
+  }
+  drawSprites();
+}
